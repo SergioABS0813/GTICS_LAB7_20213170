35e3142fa7a30ecdfecdfc49436ed2502d4e9fa6
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-05-29 20:52:39 -0500

    casi terminado el crear player

--- lab7-20213170/src/main/java/com/example/lab720213170/Controller/PlayerController.java
+++ lab7-20213170/src/main/java/com/example/lab720213170/Controller/PlayerController.java
@@ -2,12 +2,10 @@ package com.example.lab720213170.Controller;
 
 import com.example.lab720213170.Entity.Player;
 import com.example.lab720213170.Repository.PlayerRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -41,6 +39,50 @@ public class PlayerController {
     }
 
     //AGREGAR USUARIO
+
+    @PostMapping(value = {"", "/"})
+    public ResponseEntity<HashMap<String, Object>> guardarProducto(
+            @RequestBody Player player,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+        //Calculamos la posicion
+        List<Player> listaPlayersTotal = playerRepository.listaLeaderTotalporRegion(player.getRegion());
+
+        Optional<Integer> optmaxPosicion = playerRepository.obtenerMaxposition(player.getRegion());
+        int maxPosition = 1;
+        if(optmaxPosicion.isPresent()){
+            maxPosition = optmaxPosicion.get();
+        }
+
+        for(Player player1 : listaPlayersTotal){
+            if(player.getMmr() > player1.getMmr()){
+                player.setPosition(player1.getPosition());
+
+                //Actualizamos position de los demas
+                int positionPlayer1act = player1.getPosition();
+                //Listamos a los de menor e igual mmr de player1
+                List<Player> listaPlayer2 = playerRepository.listarPlayermenorMmr(player1.getRegion(), player1.getMmr());
+                for(Player player2 : listaPlayer2){
+                    player2.setPosition(player.getPosition() + 1);
+                    playerRepository.save(player2);
+                }
+
+
+            }else {
+                player.setPosition(maxPosition + 1); //Cuando el mmr es menor que todos
+            }
+        }
+        playerRepository.save(player);
+        if (fetchId) {
+            responseJson.put("idPlayer", player.getPlayerId());
+        }
+        responseJson.put("estado", "creado");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+
 
 
 
--- lab7-20213170/src/main/java/com/example/lab720213170/Repository/PlayerRepository.java
+++ lab7-20213170/src/main/java/com/example/lab720213170/Repository/PlayerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Integer> {
@@ -13,6 +14,15 @@ public interface PlayerRepository extends JpaRepository<Player,Integer> {
     //Se seleccionan solo los 10 primeros
     @Query(nativeQuery = true, value = "SELECT * FROM players where region = ? order by position limit 0, 10")
     List<Player> listarLeaderBoard(String region);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM players where region = ? order by position")
+    List<Player> listaLeaderTotalporRegion(String region);
+
+    @Query(nativeQuery = true, value = "SELECT MAX(position) FROM players where region = ?")
+    Optional<Integer> obtenerMaxposition(String region);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM players where region = ?1 and mmr <= ?2 order by position;")
+    List<Player> listarPlayermenorMmr(String region,int mmr);
 
 
 }
